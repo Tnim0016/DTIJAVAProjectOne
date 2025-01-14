f82ed73535b5ee763871fd82f7a0be24b09ab234
Tnim0016 <S6752410016@sau.ac.th>
2025-01-14 14:12:35 +0700

    v.2

---
+++ src/com/sau/tech/Myclass01.java
@@ -0,0 +1,8 @@
+package com.sau.tech;
+
+public class Myclass01 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Hi, World!");
+    }
+}
